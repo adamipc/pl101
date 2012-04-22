b2f60180ccc61aca59de78cfaf7e29f89ca5cd9e
Adam Wendt <adam@awendtconsulting.com>
2012-04-22 11:40:14 -0700

    Initial scheem language parsing

---
+++ scheem/parse.js
@@ -0,0 +1,13 @@
+var PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync('scheem.peg', 'utf-8');
+// Show the PEG grammar file
+console.log(data);
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+// Do a test
+assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+assert.deepEqual( parse("(a (b) c)"), ["a", ["b"], "c"] );
