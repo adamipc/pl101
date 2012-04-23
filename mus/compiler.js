c8d584ab9884938b7e1755eef8ba650f2e82d461
Adam Wendt <adam@awendtconsulting.com>
2012-04-23 16:06:39 -0700

    Added test compile of output of MUS parser

--- mus/compiler.js
+++ mus/compiler.js
@@ -91,5 +91,45 @@ var melody_mus =
          left: { tag: 'note', pitch: 'c4', dur: 500 },
          right: { tag: 'note', pitch: 'd4', dur: 500 } } };
 
+melody_mus = {
+   "tag": "seq",
+   "left": {
+      "tag": "repeat",
+      "section": {
+         "tag": "seq",
+         "left": {
+            "tag": "note",
+            "pitch": "a4",
+            "dur": 250
+         },
+         "right": {
+            "tag": "rest",
+            "dur": 250
+         }
+      },
+      "count": 3
+   },
+   "right": {
+      "tag": "seq",
+      "left": {
+         "tag": "par",
+         "left": {
+            "tag": "note",
+            "pitch": "d4",
+            "dur": 250
+         },
+         "right": {
+            "tag": "note",
+            "pitch": "c4",
+            "dur": 100
+         }
+      },
+      "right": {
+         "tag": "note",
+         "pitch": "b4",
+         "dur": 200
+      }
+   }
+};
 console.log(melody_mus);
 console.log(compile(melody_mus));
